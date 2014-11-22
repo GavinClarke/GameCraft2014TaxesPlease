788779a072f25199ba417327afb25e792695e52f
unknown <C00158114@itcarloe.ie>
2014-11-22 11:16:08 +0000

    base project

---
+++ TaxesPlease/Game.cpp
@@ -0,0 +1,26 @@
+#include "Game.h"
+
+
+Game::Game(SDL_Renderer* renderer,b2World* bworld) : mRenderer(renderer), mWorld(bworld){
+	
+}
+
+
+Game::~Game(void){}
+
+void Game::update(double deltaTime){	
+	float32 timeStep = deltaTime;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+	
+	mWorld->Step(timeStep, velocityIterations, positionIterations);	
+
+	SDL_RenderClear( mRenderer );
+
+	
+
+	
+	
+	
+	SDL_RenderPresent( mRenderer );
+}
---
+++ TaxesPlease/Game.h
@@ -0,0 +1,17 @@
+#include <SDL_image.h>
+#include "Box2D\Box2D.h"
+
+
+#pragma once
+class Game
+{
+private:
+	SDL_Renderer* mRenderer;
+	b2World* mWorld;
+
+public:
+	Game(SDL_Renderer* renderer, b2World* bworld);
+	~Game(void);
+	void update(double deltaTime);
+};
+
